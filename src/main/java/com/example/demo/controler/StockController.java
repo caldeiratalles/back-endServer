2f9cfb73a7d689a5516452013c9fa2797905e015
TM <64755991+caldeiratalles@users.noreply.github.com>
2023-01-26 16:28:33 -0300

    projeto finalizado

--- src/main/java/com/example/demo/controler/StockController.java
+++ src/main/java/com/example/demo/controler/StockController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/stock")
-@CrossOrigin(origins = "http://localhost:3000")
 public class StockController {
 
     private final StockService stockService;
--- src/main/java/com/example/demo/controler/UserController.java
+++ src/main/java/com/example/demo/controler/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/userStock")
-@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final StockService stockService;
